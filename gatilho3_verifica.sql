9488a5635be62acbc81efc9144ef61cefef9ce4d
DanielBradaschia <daniel.bradaschia@gmail.com>
2019-05-25 18:14:52 +0100

    verifica 3 done

---
+++ gatilho3_verifica.sql
@@ -0,0 +1,9 @@
+SELECT numberSongs 
+FROM ALBUM
+WHERE ALBUM.idAlbum = 1;
+
+INSERT INTO SONG (idSong, name, length, playCounts) VALUES (1, 'Breaking the habbit', 3, 463879000);
+
+SELECT numberSongs 
+FROM ALBUM
+WHERE ALBUM.idAlbum = 1;
