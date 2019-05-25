53b8430feb632fed5c30fb8c8a27f8ca6f0c9102
DanielBradaschia <daniel.bradaschia@gmail.com>
2019-05-25 17:51:12 +0100

    verifica 2 done?

--- criar.sql
+++ criar.sql
@@ -20,7 +20,7 @@ DROP TABLE IF EXISTS SONG;
 CREATE TABLE IF NOT EXISTS SONG(
     idSong INTEGER REFERENCES ALBUM(idAlbum),
     name STRING NOT NULL,
-    length TIME CHECK (length > 0),
+    length INTEGER,
     playCounts INTEGER CHECK (playCounts > 0)
 );
 
---
+++ gatilho2_verifica.sql
@@ -0,0 +1,11 @@
+SELECT SONG.idSong, SONG.length
+from SONG
+WHERE SONG.idSong = 1;
+
+UPDATE SONG SET length = -21 WHERE SONG.idSong = 1;
+
+SELECT SONG.idSong, SONG.length
+from SONG
+WHERE SONG.idSong = 1;
+
+UPDATE SONG SET length = 4 WHERE SONG.idSong = 1;
