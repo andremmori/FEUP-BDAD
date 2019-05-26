e6b26d52929f1350491a1588b2722c57f6c87d8d
Andre Mori <andremori16@gmail.com>
2019-05-26 14:52:05 +0100

    fixed add 2

--- gatilho2_adiciona.sql
+++ gatilho2_adiciona.sql
@@ -1,6 +1,6 @@
 CREATE TRIGGER IF NOT EXISTS SongLen
-BEFORE UPDATE OF length ON SONG
-WHEN length <= 0
+BEFORE INSERT ON SONG
+WHEN New.length <= 0
     BEGIN
         SELECT RAISE(ABORT, "Duração da música inválida.");
     END;
